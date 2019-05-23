53eb3fdbf40129199998b72762754aa2b9c5d25d
cunjj86 <ljgchunjj@naver.com>
2019-05-23 18:25:27 +0900

    로또 당첨 프로그램 완성

--- app/src/main/java/com/example/lottodevice/MainActivity.java
+++ app/src/main/java/com/example/lottodevice/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lottodevice;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -19,7 +20,6 @@ public class MainActivity extends BaseActivity {
 
     int usesPay = 0;
     int aggPickPay = 0;
-    int rank = 6;
     final int[] rankPay = new int[5];
 
     @Override
@@ -44,7 +44,9 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
 
+                int rank = 6;
                 usesPay = usesPay + 1000;
+                act.usesPayTxt.setText(String.format("사용금액 : %,d 원", usesPay));
 
                 makePickUpNumber();
 
@@ -55,22 +57,39 @@ public class MainActivity extends BaseActivity {
                 act.pickUpNumber5.setText(Integer.toString(pickUpNumberArray[4]));
                 act.pickUpNumber6.setText(Integer.toString(pickUpNumberArray[5]));
 
-                boolean isWinningOk = false;
-
                 for (int i=0; i < winningNumberArray.length; i++) {
                     if (winningNumberArray[i] == pickUpNumberArray[i]) {
-                        isWinningOk = true;
                         rank = rank - 1;
-                    }
-                    else {
-                        isWinningOk = false;
                     }
                 }
 
-                if (isWinningOk && rank == 0) {
+                Log.d("랭크값 : ", Integer.toString(rank));
+
+                if (rank == 0) {
                     Toast.makeText(mContext, "1등에 당첨되었습니다!! 축하드립니다.", Toast.LENGTH_SHORT).show();
-                    
+                    aggPickPay = aggPickPay + rankPay[0];
                 }
+                else if (rank == 1) {
+                    Toast.makeText(mContext, "2등에 당첨되었습니다!! 축하드립니다.", Toast.LENGTH_SHORT).show();
+                    aggPickPay = aggPickPay + rankPay[1];
+                }
+                else if (rank == 2) {
+                    Toast.makeText(mContext, "3등에 당첨되었습니다!! 축하드립니다.", Toast.LENGTH_SHORT).show();
+                    aggPickPay = aggPickPay + rankPay[2];
+                }
+                else if (rank == 3) {
+                    Toast.makeText(mContext, "4등에 당첨되었습니다!! 축하드립니다.", Toast.LENGTH_SHORT).show();
+                    aggPickPay = aggPickPay + rankPay[3];
+                }
+                else if (rank == 4) {
+                    Toast.makeText(mContext, "5등에 당첨되었습니다!! 축하드립니다.", Toast.LENGTH_SHORT).show();
+                    aggPickPay = aggPickPay + rankPay[4];
+                }
+                else {
+                    Toast.makeText(mContext, "아쉽네요. 다음 기회에 도전하세요!", Toast.LENGTH_SHORT).show();
+                }
+
+                act.aggPickPayTxt.setText(String.format("누적 당첨금액 : %,d 원", aggPickPay));
             }
         });
 
@@ -92,11 +111,11 @@ public class MainActivity extends BaseActivity {
 
             for (int j=0; j < tempNumber.length; j++) {
 
-                for (int k=0; k < tempNumber.length; k++) {
+                for (int k=0; k < tempNumber.length - 1; k++) {
 
                     if (tempNumber[k] == tempNumber[k+1]) {
                         isDupOk = false;
-                        Toast.makeText(mContext, "랜덤 숫자 중복 오류입니다.", Toast.LENGTH_LONG).show();
+//                        Toast.makeText(mContext, "랜덤 숫자 중복 오류입니다.", Toast.LENGTH_LONG).show();
                         break;
                     }
 
@@ -163,7 +182,7 @@ public class MainActivity extends BaseActivity {
 
                     if (tempNumber[k] == tempNumber[k+1]) {
                         isDupOk = false;
-                        Toast.makeText(mContext, "랜덤 숫자 중복 오류입니다.", Toast.LENGTH_LONG).show();
+//                        Toast.makeText(mContext, "랜덤 숫자 중복 오류입니다.", Toast.LENGTH_LONG).show();
                         break;
                     }
 
